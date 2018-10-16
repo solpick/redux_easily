7e07977d240ab652816b5f7c715152e34b2ca3c3
SOL1023 <solinepik@gmail.com>
2018-10-17 00:48:26 +0900

    주석추가

--- src/modules/input.js
+++ src/modules/input.js
@@ -4,18 +4,26 @@ import { handleActions, createAction } from "redux-actions"
 const SET_INPUT = 'input/SET_INPUT';
 // 액션 타입 형식.
 // (프로젝트/)리듀서name/액션타입
+// 기존st 코드
+// export const SET_INPUT = 'SET_INPUT' 
 
 export const setInput = createAction(SET_INPUT);
-// 액션 생성 함수 아래코드보다 편리.
+// setInput('ABC') || setInput({a: 1, b: '3'});
+// {
+//      type: 'input/SET_INPUT'   
+//      payload: 'ABC' || { a: 1, b: '3' }
+// }
+// 액션 생성 함수 기존.
 // export const setInput = (index) => ({
 //     type: types.SET_INPUT,
 //     index
 // })
 
+
 const initialState = Map({
     value: ''
 });
-// 초기값 셋팅
+// 초기값 셋팅 (immutable 사용)
 
 export default handleActions({
     [SET_INPUT]: (state, action) => {
