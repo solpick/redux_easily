499bcf085a7f8cf8101fe77f2b292535cd6a4f55
SOL1023 <solinepik@gmail.com>
2018-10-20 05:14:44 +0900

    todolist 컴포넌트

---
+++ src/containers/TodoListContainer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import TodoList from '../components/TodoList';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// 액션 생성함수 불러오기
+import * as todosActions from '../modules/todos'
+
+class TodoListContainer extends Component {
+    handleToggle = (id) => {
+        const { ToggleActions } = this.props;
+        ToggleActions.toggle(id);
+    }
+
+    handleRemove = (id) => {
+        const { TodosActions } = this.props;
+        TodosActions.remove(id);
+    }
+
+    render() {
+        const { todos } = this.props;
+        const { handleToggle, handleRemove } = this;
+        return (
+            <TodoList
+                todos={ todos }
+                onToggle={ handleToggle }
+                onRemove={ handleRemove }
+            />
+        );
+    }   
+}
+
+export default connect(
+    (state) => ({
+        todos: state.todos
+    }),
+    (dispatch) => ({
+        TodosActions: bindActionCreators(todosActions, dispatch)
+    })
+)(TodoListContainer);
